4b84ba09b4042ef16980a0e8b03740db09a3f232
codewithtabish <codewithtabish16@gmail.com>
2025-02-23 05:02:47 -1200

    basic shadecn and navbar design has been done

---
+++ src/app/(main_layout)/page.tsx
@@ -0,0 +1,16 @@
+import { Button } from '@/components/ui/button'
+import React from 'react'
+
+const HomePage = () => {
+  return (
+    <div className='py-12'>
+    
+    Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptate eaque eveniet commodi voluptatibus, inventore excepturi consectetur impedit amet officiis similique tempora fugiat voluptatum non? Suscipit cumque aperiam vitae tenetur facere!
+    Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptate eaque eveniet commodi voluptatibus, inventore excepturi consectetur impedit amet officiis similique tempora fugiat voluptatum non? Suscipit cumque aperiam vitae tenetur facere!
+    Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptate eaque eveniet commodi voluptatibus, inventore excepturi consectetur impedit amet officiis similique tempora fugiat voluptatum non? Suscipit cumque aperiam vitae tenetur facere!
+      
+    </div>
+  )
+}
+
+export default HomePage
